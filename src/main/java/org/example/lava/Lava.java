ce4cd439c5bd0419a4b1b21bd2643d1e2d6bb54b
gr40in <gr40in@yandex.ru>
2023-12-17 14:45:42 +0500

    fuck

--- src/main/java/org/example/lava/Lava.java
+++ src/main/java/org/example/lava/Lava.java
@@ -6,6 +6,8 @@ import org.example.bambam.obstacle.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Lava {
@@ -31,7 +33,7 @@ public class Lava {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        MAX_STEPS = (HEIGHT + WIGHT) * 2;
+        MAX_STEPS = (HEIGHT + WIGHT) + HEIGHT;
         MinHeatLost = getDefaultHeatLost();
 
     }
@@ -39,26 +41,57 @@ public class Lava {
 
     public static void main(String[] args) throws IOException {
         printArray(arr);
-        System.out.println(MinHeatLost);
+//        System.out.println(MinHeatLost);
+
+        lavaWay(0, 0, new LavaVector(0, 1), 1, 0, 0,new boolean[HEIGHT][WIGHT]);
     }
 
-    public static void lavaWay(int y, int x, LavaVector vector, int oneDirectionStepCount, int stepCount, int lostHeat) {
+    public static void lavaWay(int y, int x, LavaVector vector, int oneDirectionStepCount, int stepCount, int lostHeat, boolean[][] alreadyPassed) {
         if (!inCity(y, x)) return;
+        boolean[][] matrix = copyArr(alreadyPassed);
+        if (matrix[y][x]) return;
+        matrix[y][x] = true;
+        if (oneDirectionStepCount > 3) return;
         lostHeat += arr[y][x];
         if (lostHeat > MinHeatLost && stepCount > MAX_STEPS) return;
 
         if (y == HEIGHT - 1 && x == WIGHT - 1) {
-            System.out.println(" we found some way - and lost only " + lostHeat + " heat");
+            if (lostHeat < MinHeatLost) {
+                System.out.println(" we found some way - and lost only " + lostHeat + " heat");
+            }
             return;
         }
-//        lavaWay();
-
+        for (LavaVector newLavaVector : getAllActualWays(vector, oneDirectionStepCount)) {
+            lavaWay(y + newLavaVector.yDirection, x + newLavaVector.xDirection, newLavaVector,
+                    newLavaVector.equals(vector) ? oneDirectionStepCount + 1 : 1, stepCount++, lostHeat, matrix);
+        }
 
 
     }
 
-    public static LavaVector[] getAllActualWays(LavaVector vector){
-        return new LavaVector[3];
+    public static boolean[][] copyArr(boolean[][] array) {
+        boolean[][] tempLink = new boolean[HEIGHT][WIGHT];
+        for (int y = 0; y < HEIGHT; y++) {
+            for (int x = 0; x < WIGHT; x++) {
+                tempLink[y][x] = array[y][x];
+            }
+
+        }
+        return tempLink;
+    }
+
+    public static List<LavaVector> getAllActualWays(LavaVector vector, int count) {
+        List<LavaVector> tempList = new ArrayList<>();
+        if (count < 3) tempList.add(new LavaVector(vector.yDirection, vector.xDirection));
+        if (vector.xDirection == 0) {
+            tempList.add(new LavaVector(0, -1));
+            tempList.add(new LavaVector(0, 1));
+        }
+        if (vector.yDirection == 0) {
+            tempList.add(new LavaVector(-1, 0));
+            tempList.add(new LavaVector(1, 0));
+        }
+        return tempList;
     }
 
     public static boolean inCity(int y, int x) {
